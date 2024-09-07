6261fc8e9ef04e8af7488e1f92f234a30b33b1bd
阿朱quq <785916801@qq.com>
2024-09-08 01:59:28 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '38': '9.5rem',
+      }
+    },
   },
   plugins: [],
 }

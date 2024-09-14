3fd4686f402d14f58a7cb762bb2a04aa55204d77
阿朱quq <785916801@qq.com>
2024-09-15 02:19:28 +0800

    Update getAll.js

--- src/api/getAll.js
+++ src/api/getAll.js
@@ -8,7 +8,7 @@ export const getAllRoute = (res = {}) => {
         companyNo: '171020091821002',
         scontent: '99999',
     }
-    return getAll.post(
+    return wechatHttp.post(
         Api.getRouteDetail,
         `params=${JSON.stringify(data)}`
     )
